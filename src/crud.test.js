f421ce1a39f3972ce156bfa0177f90e971ddafb1
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-19 11:12:47 -0600

    Test that task was added to empty task container

--- src/crud.test.js
+++ src/crud.test.js
@@ -2,6 +2,18 @@ import { addTask, deleteSelectedTask } from './crud-module';
 //add and remove functions
 
 describe('Add task function tests', () => {
+  test('added one new task to empty task container', () => {
+    //ARRANGE
+    const task = { description: 'abc' };
+    const tasksContainer = [];
+
+    //ACT
+    addTask(task.description, tasksContainer);
+
+    //ASSERT
+    expect(tasksContainer.length).toBe(1);
+  });
+
   test('New task description inserted in task container is abc', () => {
     // ARRANGE
     const task = { description: 'abc' };
